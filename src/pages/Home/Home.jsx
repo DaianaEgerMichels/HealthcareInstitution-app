cb726a3314d3cb671177aa9f54e40e80da1e53a2
Daiana Michels <daianaem.12@gmail.com>
2022-07-28 22:41:54 -0300

    chore: change application name and catch return

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,48 +1,53 @@
-import React , {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 import Navbar from "../../components/Navbar/Navbar";
-import api from '../../utils/api';
-
+import api from "../../utils/api";
 
 function Home() {
-    const [balance, setBalance] = useState(0);
+  const [balance, setBalance] = useState(0);
 
-    useEffect(() => {
-      const institutionLoggedString = localStorage.getItem('_institution_logged')
-      const institutionLogged = JSON.parse(institutionLoggedString)
-      api.get(`/api/healthcare-institutions/${institutionLogged.id}`
-    ).then(response => {
-      setBalance(response.data.pixeonCoins);
-    }).catch(erro =>
-      console.log(erro.response)
-    )
+  useEffect(() => {
+    const institutionLoggedString = localStorage.getItem("_institution_logged");
+    const institutionLogged = JSON.parse(institutionLoggedString);
+    api
+      .get(`/api/healthcare-institutions/${institutionLogged.id}`)
+      .then((response) => {
+        setBalance(response.data.pixeonCoins);
+      })
+      .catch((erro) => alert(erro.response));
   }, [balance]);
-
   return (
     <>
-    <Navbar/>
-    <div className="container">
-    <div className="jumbotron">
-    <h1 className="display-3">Welcome!</h1>
-    <p className="lead">This is your healthcare system.</p>
-    <p className="lead">You have {balance} Pixeon Coins to use in the system.</p>
-    <hr className="my-4" />
-    <p>And this is your administrative area, use one of the menus or buttons below to navigate the system.</p>
-    <p className="lead">
-        <a className="btn btn-success btn-lg" 
-        href="/register-exam" 
-        role="button"><i className="pi pi-users"></i>  
-         Register Exam
-        </a>
-        <a className="btn btn-primary btn-lg" 
-        href="/exams" 
-        role="button"><i className="pi pi-money-bill"></i>  
-         Registered Exams
-        </a>
-    </p>
-</div>
-</div>
-</>
-  )
+      <Navbar />
+      <div className="container">
+        <div className="jumbotron">
+          <h1 className="display-3">Welcome!</h1>
+          <p className="lead">This is your healthcare system.</p>
+          <p className="lead">
+            You have {balance} Pixeon Coins to use in the system.
+          </p>
+          <hr className="my-4" />
+          <p>
+            And this is your administrative area, use one of the menus or
+            buttons below to navigate the system.
+          </p>
+          <p className="lead">
+            <a
+              className="btn btn-success btn-lg"
+              href="/register-exam"
+              role="button"
+            >
+              <i className="pi pi-users"></i>
+              Register Exam
+            </a>
+            <a className="btn btn-primary btn-lg" href="/exams" role="button">
+              <i className="pi pi-money-bill"></i>
+              Registered Exams
+            </a>
+          </p>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Home;
